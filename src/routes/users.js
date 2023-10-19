66b73da45e1005730fd23ca6292e8e654f3131b9
VatthanhunV <vatthanhun@gmail.com>
2023-10-19 21:26:35 +0700

    user auth

---
+++ src/routes/users.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import {
+    getUsersController,
+    registerUsersController,
+    loginUsersController,
+    getCurrentUserController,
+    updateUsersController,
+    deleteUsersController
+} from '../controllers/usersController.js';
+const usersRouter = express.Router();
+
+usersRouter.get("/", getUsersController);
+usersRouter.post("/register", registerUsersController);
+usersRouter.post("/login", loginUsersController);
+usersRouter.get("/:id", getCurrentUserController);
+usersRouter.put("/:id", updateUsersController);
+usersRouter.delete("/:id", deleteUsersController);
+
+export default usersRouter;
